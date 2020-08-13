b756c57f35c5eeddab7dac212c127ec9e1adae03
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-13 14:17:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var kittens = ["Milo","Otis","Garfield"]; //define your array here
 var array2=new array();
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-return kittens.push();
+return kittens.push(name);
 }
 
 function destructivelyPrependKitten(name){
-  return kittens.unshift();
+  return kittens.unshift(name);
 }
 
 function destructivelyRemoveLastKitten(){
@@ -18,12 +18,12 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  array2=kittens.push();
+  array2=kittens.push(name);
   return array2;
 }
 
 function prependKitten(name){
-  array2=kittens.unshift();
+  array2=kittens.unshift(name);
   return array1;
 }
 
